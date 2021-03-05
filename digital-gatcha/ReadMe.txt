382d7c1d566d8ef005213fe8ef481e6ff3b68f72
katiesaurus <80079617+katiesaurus@users.noreply.github.com>
2021-03-04 17:50:10 -0800

    Add files via upload

---
+++ digital-gatcha/ReadMe.txt
@@ -0,0 +1,7 @@
+Please edit these files in Atom or a similar text editor. DO NOT USE NOTEPAD OR WORD. It messes with the quotation marks.
+
+Download Atom at atom.io.
+
+I use Github for hosting because I am cheap and lazy and Github has good version control.
+
+If you use Github, give the entire folder the name you want your page ID to have. For example, if your page is going to be klongua.github.io/drawingprompts, then name this folder drawingprompts.
---
+++ digital-gatcha/script.js
@@ -0,0 +1,54 @@
+//This is your javascript code. This is where the math happens!
+//setWords() is the command you'll execute when someone pushes the button. Here's where we define it.
+setWords()
+
+//double check that your choices are formatted correctly, with a pair of STRAIGHT (not curly) quotation marks and a comma OUTSIDE the quotation marks.
+function setWords() {
+  const choice1 = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violent",
+    "pink",
+    "black",
+    "teal",
+  ];
+
+  const choice2 = [
+    "floral",
+    "starry",
+    "plaid",
+    "paisley",
+    "polka dotted",
+    "striped",
+    "animal patterned",
+    "chevron",
+    "houndstooth",
+    "checkered",
+    "ombre",
+  ];
+
+   const choice3 = [
+     "horror",
+     "terror",
+     "beast",
+     "monster",
+     "creature",
+     "behemoth",
+     "demon",
+     "mutant",
+     "abomination",
+      ];
+
+  const randomNumber1 = Math.floor(Math.random() * choice1.length);
+  const randomNumber2 = Math.floor(Math.random() * choice2.length);
+   const randomNumber3 = Math.floor(Math.random() * choice3.length);
+
+  document.getElementById("choice1").innerHTML = choice1[randomNumber1];
+  document.getElementById("choice2").innerHTML = choice2[randomNumber2];
+   document.getElementById("choice3").innerHTML = choice3[randomNumber3];
+
+}
